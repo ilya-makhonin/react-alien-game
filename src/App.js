b238b148d9428b8a2cd0a35fd10718529787b121
Ilia Makhonin <noizemkutane77@gmail.com>
2018-10-22 03:17:21 +0300

    В файл внесены изменения

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import './styles/App.scss';
 import PropTypes from 'prop-types';
 import { getCanvasPosition } from './utils/formula';
 import Canvas from './components/Canvas';
@@ -10,6 +9,13 @@ class App extends Component {
         setInterval(() => {
             self.props.moveObjects(self.canvasMousePosition);
         }, 10);
+
+        window.onresize = () => {
+            const cnv = document.getElementById('aliens-go-home-canvas');
+            cnv.style.width = `${window.innerWidth}px`;
+            cnv.style.height = `${window.innerHeight}px`;
+        };
+        window.onresize();
     }
 
     trackMouse(event) {
@@ -20,6 +26,8 @@ class App extends Component {
         return (
             <Canvas
                 angle={this.props.angle}
+                gameState={this.props.gameState}
+                startGame={this.props.startGame}
                 trackMouse={event => (this.trackMouse(event))}
             />
         );
@@ -28,7 +36,20 @@ class App extends Component {
 
 App.propTypes = {
     angle: PropTypes.number.isRequired,
+    gameState: PropTypes.shape({
+        started: PropTypes.bool.isRequired,
+        kills: PropTypes.number.isRequired,
+        lives: PropTypes.number.isRequired,
+        flyingObjects: PropTypes.arrayOf(PropTypes.shape({
+            position: PropTypes.shape({
+                x: PropTypes.number.isRequired,
+                y: PropTypes.number.isRequired
+            }).isRequired,
+            id: PropTypes.number.isRequired,
+        })).isRequired,
+    }).isRequired,
     moveObjects: PropTypes.func.isRequired,
+    startGame: PropTypes.func.isRequired,
 };
 
 export default App;
